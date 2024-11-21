c48c9c8f28f7e420cfbbc00ef48a7b228a0ea096
Daniel Jansson <148873761+MRDJansson@users.noreply.github.com>
2024-11-21 17:31:26 +0200

    Projektin aloitus

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import AddCourseNotes from './AddCourseNotes'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/Courses.jsx
+++ src/Courses.jsx
@@ -4,13 +4,13 @@ import Notes from "./Notes";
 import { useStore } from "./store/Store";
 
 function Courses() {
-  const lista = useStore((state) => state.lista); // Access shared state
+  const lista = useStore((state) => state.lista);
 
   return (
     <div>
       <ul>
         {lista.map((kurssi, i) => (
-          <Notes key={i} name={kurssi} /> // Correctly render Notes component
+          <Notes key={i} name={kurssi} />
         ))}
       </ul>
     </div>
